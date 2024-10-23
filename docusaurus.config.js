411fa47d2ca80b14ea41f14219f9d68aebaf47bf
Mousum borah <mousumborah8@gmail.com>
2024-10-23 21:47:39 +0530

    update config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'BST', // Usually your GitHub org/user name.
   projectName: 'BST_docusaurus', // Usually your repo name.
-  deploymentBranch: "gh-pages"
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
